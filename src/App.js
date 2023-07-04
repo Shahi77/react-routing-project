7fd64fbbdc61183637b4ab12f9fa4c05639f819d
Shahi77 <shahiinfinity02@gmail.com>
2023-07-04 16:34:13 +0530

    adding more componets & creating_wrapper_components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetups from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import Favorites from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route path='/' exact>
           <AllMeetups />
@@ -19,7 +18,8 @@ function App() {
           <Favorites />
         </Route>
       </Switch>
-    </div>
+    </Layout>
+
   )
 }
 
